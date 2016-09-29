c60854a9ded963b5556804ec6fb81e29bed10bec
lebrun-x <lebrun_x@epita.fr>
2016-09-29 16:02:36 +0200

    [ENVIRONMENT] Set environment of back office

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,15 +1,17 @@
-package ursi.backoffice.controller;
+package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 /**
  * Created by claebo_c on 13/09/16.
  */
 @Controller
 public class HomeController {
-    @RequestMapping(value="/")
-    public String home(){
+
+    @RequestMapping(value = {"/", "/home"})
+    public String home() {
         return "index";
     }
 }
--- src/main/webapp/WEB-INF/view/404.jsp
+++ src/main/webapp/WEB-INF/view/404.jsp
@@ -1,16 +1,16 @@
 <%--
   Created by IntelliJ IDEA.
-  User: claebo_c
-  Date: 13/09/16
-  Time: 11:59
+  User: Xavier
+  Date: 29/09/2016
+  Time: 15:05
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
-    <title></title>
-  </head>
-  <body>
-test
-  </body>
+<head>
+    <title>Ceci est une erreur</title>
+</head>
+<body>
+
+</body>
 </html>
